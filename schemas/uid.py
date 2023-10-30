ded606fd8ccef70d956fe848bf522bf88415a70b
lephuhung <lephuhung77@gmail.com>
2023-10-30 17:13:47 +0700

    update v1.1

--- schemas/uid.py
+++ schemas/uid.py
@@ -1,4 +1,20 @@
-from pydantic import  BaseModel
+from pydantic import  BaseModel, Integer, String
+from sqlalchemy import Boolean, Column, DateTime, String, Integer, ForeignKey, Nullable
+from typing import Optional
 
 class uid(BaseModel):
-    pass
+    id : Optional[int]
+    uid : Optional[str]
+    name : Optional[str]
+    reaction : Optional[int]
+    SDT: Optional[str]
+    trangthai_id : Optional[int]
+    type_id : Optional [int]
+    ghichu : Optional[str]
+    Vaiao: Boolean
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = Column(
+        DateTime,
+        default=datetime.datetime.utcnow,
+        onupdate=datetime.datetime.utcnow,
+    )
