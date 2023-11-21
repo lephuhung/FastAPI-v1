14e76e917870d70709f03f72d1bc1a4a327f4d03
lephuhung <lephuhung77@gmail.com>
2023-11-21 07:58:05 +0700

    update api_router to main

--- app/Routes/pages.py
+++ app/Routes/pages.py
@@ -1,2 +1,19 @@
 from fastapi import APIRouter, Body, Depends, HTTPException, Security
-router = APIRouter(prefix="/accounts", tags=["accounts"])
+from typing import Annotated
+router = APIRouter(prefix="/uid", tags=["uid"])
+
+@router.get("/")
+async def get():
+    return {'123':'123'}
+
+@router.post("/")
+async def post():
+    return {'123':'123'}
+
+@router.put("/{uid}")
+async def update():
+    return {'123':'123'}
+
+@router.delete("/{uid}")
+async def delete(uid: Annotated[int, 0]):
+    return {'123':uid}
--- app/main.py
+++ app/main.py
@@ -1,36 +1,15 @@
-# # from fastapi import APIRouter, Depends
-# from app.Routes.api import api_router
-# # router= APIRouter()
-# # router.include_router(api_router)
-# # router.include_router(page)
-# from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
-
-# # from app.api.api_v1.api import api_router
-# # from app.core.config import settings
-
-# app = FastAPI(
-#     title='123',
-#     openapi_url=f"/api/openapi.json",
-# )
-# @router('/')
-# # app.add_middleware(
-# #     CORSMiddleware,
-# #     allow_origins=["*"],
-# #     allow_credentials=True,
-# #     allow_methods=["*"],
-# #     allow_headers=["*"],
-# # )
-
-# # app.include_router(api_router)
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
+from app.Routes.api import api_router
 app = FastAPI(title="FastAPI, Docker, and Traefik")
-
-
-@app.get("/")
-async def read_root():
-    return {'123':'123'}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.include_router(api_router, prefix='/api')
 
 
 # @app.on_event("startup")
