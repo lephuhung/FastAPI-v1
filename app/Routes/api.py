93e15dba1ac5ec587f2c02c992115dd89f352243
lephuhung <lephuhung77@gmail.com>
2023-11-22 22:31:03 +0700

    update get_current_user

--- app/Routes/api.py
+++ app/Routes/api.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
-from app.Routes import pages
+from app.Routes.router_api import uid
+from apo.Routes.router_api import user
 api_router = APIRouter()
 
-api_router.include_router(pages.router)
+api_router.include_router(uid.router)
+api_router.include_router(user.router)
---
+++ app/Routes/router_api/user.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Body, Depends, HTTPException, Security
+from typing import Annotated
+router = APIRouter(prefix="/user", tags=["user"])
+
+@router.get("")
+async def get():
+    return {'123':'123'}
+
+@router.post("")
+async def post():
+    return {'123':'123'}
+
+@router.put("/{uid}")
+async def update():
+    return {'123':'123'}
+
+@router.delete("/{uid}")
+async def delete(uid: Annotated[int, 0]):
+    return {'123':uid}
