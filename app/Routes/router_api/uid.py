4b93e371a6d8ae814b429a5141094d67dcf4f580
lephuhung <lephuhung77@gmail.com>
2024-01-01 13:45:43 +0700

    update uid

--- app/Routes/router_api/uid.py
+++ app/Routes/router_api/uid.py
@@ -4,20 +4,21 @@ from app.schemas.uid import uidCreate, uidUpdate
 from app.crud import crud_uid
 from app.Routes import deps
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import text
 router = APIRouter(prefix="/uid", tags=["UID"])
 
-@router.get("")
-async def get():
-    return {'123':'123'}
+@router.get("/get-last/{id}")
+async def get(id: str, db: Session = Depends(deps.get_db)):
+    data = crud_uid.get_last_uid_by_id( id=id, db=db)
+    return data
+
 
 @router.post("/create")
 async def create(uid: uidCreate, db: Session = Depends(deps.get_db)):
     return crud_uid.create(db=db, obj_in=uid)
 
-@router.put("/update/{uid}")
-async def update():
-    return {'123':'123'}
-
-@router.delete("/{uid}")
-async def delete(uid: Annotated[int, 0]):
-    return {'123':uid}
+@router.put("/update/{id}")
+async def update(id: int, uid_data: uidUpdate  ,db: Session = Depends(deps.get_db)):
+    uid_has_update = crud_uid.get_uid_by_id(id= id, db=db)
+    data = crud_uid.update(obj_in=uid_data, db_obj=uid_has_update, db=db)
+    return data
--- app/crud/crud_uid.py
+++ app/crud/crud_uid.py
@@ -1,7 +1,16 @@
 from app.crud.base import CRUDBase
 from app.models.uid import uid
 from app.schemas.uid import uidCreate, uidUpdate
+from sqlalchemy.orm import Session
+
 class CRUDUid (CRUDBase[uid,uidCreate, uidUpdate]):
-    pass
+    def get_uid_by_id(self, id:int, db: Session):
+        data = db.query(uid).filter(uid.id == id).first()
+        return data 
+
+    def get_last_uid_by_id(self, id:str, db: Session):
+        data = db.query(uid).filter(uid.uid == id).last()
+        return data 
+
 
 crud_uid = CRUDUid(uid)
