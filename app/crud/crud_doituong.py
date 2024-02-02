c6ed3c4b262d77706868ae1fc5c60f4212af7edf
lephuhung <lephuhung77@gmail.com>
2024-02-02 08:30:43 +0700

    update crud-doituong

--- app/crud/crud_doituong.py
+++ app/crud/crud_doituong.py
@@ -1,24 +1,49 @@
 from  app.crud.base import CRUDBase
 from pydantic import UUID4
 from app.models.doituong import Doituong
+from app.models.doituong_uid import Doituong_UID
+from app.models.trichtin import trichtin
+from app.models.moiquanhe import moiquanhe
+from app.models.uid import uid
 from sqlalchemy.orm import Session
-from sqlalchemy import func
+from sqlalchemy import func, desc
 from app.schemas.doituong import doituongcreate, doituongupdate
 from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 class CRUD_DOITUONG (CRUDBase[Doituong, doituongcreate, doituongupdate]):
     def get_doituong_by_id(self, doituong_id: UUID4, db: Session):
         return db.query(Doituong).filter(Doituong.id == doituong_id).first()
 
     def get_details(self, doituong_id: UUID4, db:Session):
-        details_count = db.query(Doituong.id.label('id'), Doituong.client_name.label('name'), func.count(Doituong.id).label('count')).filter(Doituong.id == doituong_id).group_by(Doituong.id)
+        details_uid = (
+            db.query(
+                Doituong_UID.doituong_id.label('id'),
+                Doituong.client_name.label('name'),
+                moiquanhe.name.label('moiquanhe_name'),
+                uid.uid.label('uid'),
+                uid.type_id.label('type_id'),
+                uid.name.label('uid_name')
+            )
+            .filter(Doituong_UID.doituong_id == doituong_id)
+            .join(Doituong, Doituong_UID.doituong_id == Doituong.id)
+            .join(moiquanhe, moiquanhe.id == Doituong_UID.Moiquanhe_id)
+            .join(uid, uid.uid == Doituong_UID.uid)
+            .distinct(uid.uid)
+            )
         formatted_result = [
         {
             'id': str(row.id),
             'name': row.name,
-            'count': row.count
-
+            'uid': row.uid,
+            'type_id': row.type_id,
+            'uid_name': row.uid_name,
+            'moiquanhe_name': row.moiquanhe_name
         }
-        for row in details_count
+        for row in details_uid
         ]
-        return JSONResponse(content=formatted_result)
+        count_trichtin = (db.query(func.count(trichtin.uid).label('count')).filter(trichtin.uid == str(doituong_id)).all())
+        details_trichtin = (db.query(trichtin).filter(trichtin.uid == str(doituong_id)).order_by(desc(trichtin.updated_at))).limit(5).all()
+        # return JSONResponse(content=formatted_result)
+        count = [{'doituong_id': str(doituong_id),'count': row.count} for row in count_trichtin]
+        return JSONResponse(content={'trichtin_count': count, 'hoinhom_details': formatted_result, 'trichtin_details': jsonable_encoder(details_trichtin)})
 crud_doituong = CRUD_DOITUONG(Doituong)
