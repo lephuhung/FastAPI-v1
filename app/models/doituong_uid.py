a16e281e0c0c064a3bd5e438d9a18bea48d0b9eb
lephuhung <lephuhung77@gmail.com>
2024-05-06 15:38:39 +0700

    update but not work

--- app/models/doituong_uid.py
+++ app/models/doituong_uid.py
@@ -11,10 +11,10 @@ class Doituong_UID(Base):
     """
     Database model for an Doituong UUID
     """
-    __tablename__="Doituong_UID"
+    __tablename__="doituong_uid"
     id = Column(Integer, primary_key=True, index=True, nullable=False, autoincrement=True)
-    doituong_id = Column(Integer, ForeignKey("Doituong.id"))
-    uid = Column(String(20), ForeignKey("UID.uid") )
+    doituong_id = Column(Integer, ForeignKey("doituong.id"))
+    uid = Column(String(20), ForeignKey("uid.uid") )
     moiquanhe_id= Column(Integer, ForeignKey("moiquanhe.id"))
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
     updated_at = Column(
