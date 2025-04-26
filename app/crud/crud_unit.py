c4f591184c79fa7fb37240ccce62e62c22918f9a
lephuhung <lephuhung77@gmail.com>
2025-04-26 16:35:11 +0700

    update crud_unit

--- app/crud/crud_unit.py
+++ app/crud/crud_unit.py
@@ -1,13 +1,23 @@
-from typing import Any, Dict, Optional, Union
+from typing import Any, Dict, Optional, Union, List
 from sqlalchemy.orm import Session
 from app.crud.base import CRUDBase
 from app.models.unit import Unit
 from app.schemas.unit import UnitCreate, UnitUpdate
+from app.models.user_unit import UserUnit
 
 
 class CRUDUnit(CRUDBase[Unit, UnitCreate, UnitUpdate]):
     def get_by_name(self, db: Session, *, name: str) -> Optional[Unit]:
         return db.query(Unit).filter(Unit.name == name).first()
+    
+    def get_by_user_id(self, db: Session, *, user_id: str) -> Optional[Unit]:
+        # Lấy user_unit từ bảng user_units
+        user_unit = db.query(UserUnit).filter(UserUnit.user_id == user_id).first()
+        if not user_unit:
+            return None
+        
+        # Lấy unit từ bảng units
+        return db.query(Unit).filter(Unit.id == user_unit.unit_id).first()
 
 
 unit = CRUDUnit(Unit) 
